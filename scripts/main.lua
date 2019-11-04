7793d9a7304039798551f0dc2230ee74566d598e
Prévotat Jérôme <jer.prevotat@gmail.com>
2019-11-04 22:23:31 +0100

    Slash Commands added
    Added : 
    - /allmyid to see all your IDs
    - /whocan Instance_Name to see wich character is free to do a particular instance. (doesnt take difficulty in account yet !)

--- scripts/main.lua
+++ scripts/main.lua
@@ -8,7 +8,7 @@ EventFrame:SetScript("OnEvent", function (this, event, ...)
 	ID_Tracker[event](ID_Tracker, ...)
 end);
 
-function ID_Tracker:PLAYER_ENTERING_WORLD(init)
+function ID_Tracker:PLAYER_ENTERING_WORLD()
 
 	self:SetDefaults()
 	local i = 1;
@@ -64,4 +64,42 @@ function ID_Tracker:SetDefaults()
 		}
 	end;
 	return true;
+end
+
+SLASH_PRINT_ALL_ID1 = "/allmyid";
+SLASH_FIND_FREE_BY_INSTANCE_NAME1 = "/whocan"
+
+SlashCmdList["FIND_FREE_BY_INSTANCE_NAME"] = function (instance_name)
+	local str = "";
+	local character_free = true;
+	local i = 1;
+	for key,value in pairs(id_table) do
+		if ((#id_table[key]) == 0) then str = str .. key .. "\n"
+		else
+			i = 1;
+			character_free = true;
+			while (i <= (#id_table[key])) do
+				if (id_table[key][i]["name"] == instance_name)
+				then character_free = false end 
+				i = i + 1;
+			end
+			if character_free then str = str .. key .. "\n" end
+		end
+	end
+	if str == "" then str = "All your characters are tag !" end
+	message(str);
+end
+
+SlashCmdList["PRINT_ALL_ID"] = function ()
+	local str = "";
+	local i = 1;
+	for key,value in pairs(id_table) do
+		str = str .. key .. ":\n"
+		i = 1;
+		while (i <= (#id_table[key])) do
+			str = str .. id_table[key][i]["name"] .. " " .. id_table[key][i]["difficultyName"] .. "\n"; 
+			i = i + 1;
+		end
+	end
+	message(str);
 end
